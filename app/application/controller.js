e403fe7c7bb9bf079cf14c37466b229e044dc551
machty <machty@gmail.com>
2016-02-22 10:27:03 -0500

    [FEATURE]: initial commit for code camps

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,16 +1,64 @@
 import Ember from 'ember';
 import SlideComponent from 'ec-prezo/components/slide';
+import { task, subscribe, events } from 'ember-concurrency';
 
 const { computed } = Ember;
+const LEFT = 37;
+const RIGHT = 39;
+const SPACEBAR = 32;
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(Ember.Evented, {
   queryParams: ['slide'],
   slide: 'home',
 
   slides: [
     "home",
-    "intro",
-    "hello",
+    "dali",
+    "dali-tomster",
+    "ec-intro-1",
+    "ec-intro-2",
+    "structured-programming-1",
+    "structured-programming-2",
+    "js-structured-programming-1",
+    "js-structured-programming-2",
+    "js-structured-programming-3",
+    "js-structured-programming-4",
+    "js-structured-programming-5",
+    "js-structured-programming-6",
+    "preventing-concurrency-1",
+    "preventing-concurrency-2",
+    "whackamole",
+    "structured-concurrency-1",
+    "structured-concurrency-2",
+    "enter-ember-concurrency-1",
+    "enter-ember-concurrency-2",
+    "enter-ember-concurrency-3",
+    "enter-ember-concurrency-4",
+    "enter-ember-concurrency-5",
+    "enter-ember-concurrency-6",
+    "things-that-suck-1",
+    "what-about-concurrency-1",
+    "what-about-concurrency-2",
+    "what-about-concurrency-3",
+    "what-about-concurrency-4",
+    "what-about-concurrency-5",
+    "what-about-concurrency-6",
+    "task-modifiers-1",
+    "task-modifiers-2",
+    "task-modifiers-3",
+    "task-modifiers-4",
+    "task-modifiers-5",
+    "task-modifiers-6",
+    "task-modifiers-7",
+    "things-that-rock-1",
+    "accelerator-example",
+    "autocomplete-example-1",
+    "autocomplete-example-2",
+    "many-examples",
+    "advanced-topics",
+    "summary",
+    "go-alone",
+    "thank-you",
   ],
 
   indexedSlides: computed('slides', function() {
@@ -26,7 +74,8 @@ export default Ember.Controller.extend({
         componentName,
         name,
         prev,
-        next
+        next,
+        index: i,
       });
     }
     return indexedSlides;
@@ -38,6 +87,11 @@ export default Ember.Controller.extend({
     return indexedSlides.findBy('name', name) || indexedSlides[0];
   }),
 
+  progress: computed('currentSlide', function() {
+    let slide = this.get('currentSlide');
+    return slide.index / (this.slides.length - 1);
+  }),
+
   registerAndValidateSlide(componentName) {
     let container = Ember.getOwner(this);
     let componentClassName = `component:${componentName}`;
@@ -47,6 +101,46 @@ export default Ember.Controller.extend({
     if (!ComponentClass) {
       container.register(componentClassName, SlideComponent);
     }
+  },
+
+  index: 0,
+  keyboardListener: task(function * () {
+    yield subscribe(events(Ember.$(document.body), 'keydown'), function * (ev) {
+      let { keyCode } = ev;
+      let destination;
+      if (keyCode === LEFT) {
+        destination = this.get('currentSlide.prev');
+        if (destination) {
+          this.set('index', 0);
+          this.set('slide', destination);
+        }
+      } else if (keyCode === RIGHT || keyCode === SPACEBAR) {
+        this.advance();
+      }
+    });
+  }).on('init'),
+
+  advance() {
+    let destination = this.get('currentSlide.next');
+    document.getSelection().removeAllRanges();
+    let prevented = false;
+    let proceedEvent = {
+      preventDefault() {
+        let oldPrevented = prevented;
+        prevented = true;
+        return !oldPrevented;
+      },
+      index: this.index,
+    };
+    this.trigger('nextSlide', proceedEvent);
+    Ember.run.next(() => {
+      if (!prevented && destination) {
+        this.set('index', 0);
+        this.set('slide', destination);
+      } else {
+        this.incrementProperty('index');
+      }
+    });
   },
 });
 
--- app/components/slide-container/component.js
+++ app/components/slide-container/component.js
@@ -1,21 +1,24 @@
 import Ember from 'ember';
-import { task, timeout, subscribe, events } from 'ember-concurrency';
+import { task, timeout, subscribe, events, race } from 'ember-concurrency';
 
 const BASE_WIDTH  = 900;
 const BASE_HEIGHT = 500;
 
 export default Ember.Component.extend({
+  currentSlide: null,
+  progress: null,
+
   classNames: 'slide-container',
 
   viewportListener: task(function * () {
-    this.updateWindowSize();
+    this.updateContainerSize();
     yield subscribe(events(Ember.$(window), 'resize'), function * () {
       yield timeout(200);
-      this.updateWindowSize();
+      this.updateContainerSize();
     }).restartable();
   }).on('didInsertElement'),
 
-  updateWindowSize() {
+  updateContainerSize: Ember.observer('progress', function() {
     let height = window.innerHeight;
     let width  = window.innerWidth;
     let baseRatio = BASE_WIDTH / BASE_HEIGHT;
@@ -25,11 +28,20 @@ export default Ember.Component.extend({
       width / BASE_WIDTH :
       height / BASE_HEIGHT;
 
+    let color = `hsl(${this.get('progress')*180}, 100%, 95%)`;
+
     this.$().css({
+      backgroundColor: color,
       height: BASE_HEIGHT,
       width: BASE_WIDTH,
       transform: ` translateX(-50%) translateY(-50%) scale(${trans})`,
     });
-  }
+  }),
+
+  click: function(ev) {
+    if (Ember.$(ev.target).closest('a,button').length === 0) {
+      this.sendAction('advance');
+    }
+  },
 });
 
